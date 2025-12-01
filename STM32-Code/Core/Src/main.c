95a261089e770d28804c8ddfd373ee51cbc2115a
GavinTunnell <gavintunnell20@gmail.com>
2025-11-30 19:25:33 -0600

    Update main.c

--- STM32-Code/Core/Src/main.c
+++ STM32-Code/Core/Src/main.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
+I2C_HandleTypeDef hi2c1;
 TIM_HandleTypeDef htim1;
 
 UART_HandleTypeDef huart1;
-UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart5;
 UART_HandleTypeDef huart3;
 
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_USART2_UART_Init(void);
+static void MX_UART5_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART3_UART_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_I2C1_Init(void);
 void UState_Machine(void);
 void CState_Machine(void);
 
@@ -24,6 +26,9 @@ unsigned short voltage;
 unsigned long current;
 unsigned long watts;
 unsigned long watthours;
+unsigned char pzem = 1;
+unsigned char pzem_active = 1;
+unsigned char timeout;
 
 enum {UInit, UWait, UTransmit, URecieve} UARTState;
 
@@ -39,7 +44,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-    if (huart->Instance == USART1)
+    if (huart->Instance == USART1 || huart->Instance == UART5 || huart->Instance == USART3)
     {
         rx_done = 1;
     }
@@ -50,10 +55,11 @@ int main(void)
     HAL_Init();
     SystemClock_Config();
     MX_GPIO_Init();
-    MX_USART2_UART_Init();
+    MX_UART5_Init();
     MX_USART1_UART_Init();
     MX_USART3_UART_Init();
     MX_TIM1_Init();
+    MX_I2C1_Init();
 
     HAL_TIM_Base_Start_IT(&htim1);
 
@@ -75,6 +81,7 @@ void UState_Machine(void)
     switch (UARTState)
     {
         case UInit:
+        	timeout = 0;
             rx_done = 0;
             UARTState = UWait;
         break;
@@ -82,45 +89,117 @@ void UState_Machine(void)
             UARTState = UTransmit;
         break;
         case UTransmit:
-            if (HAL_UART_Transmit(&huart1, request, 8, 50) == HAL_OK)
-            {
-                HAL_UART_Receive_IT(&huart1, (unsigned char*)rx_data, 25);
-                UARTState = URecieve;
+        	if (pzem == 1)
+        	{
+                if (HAL_UART_Transmit(&huart1, request, 8, 50) == HAL_OK)
+                {
+                	pzem_active = 1;
+                    HAL_UART_Receive_IT(&huart1, (unsigned char*)rx_data, 25);
+                    UARTState = URecieve;
+                }
+                else
+                {
+                    UARTState = UWait;
+                }
             }
-            else
-            {
-                UARTState = UWait;
-            }
+        	else if (pzem == 2)
+        	{
+                if (HAL_UART_Transmit(&huart5, request, 8, 50) == HAL_OK)
+                {
+                	pzem_active = 2;
+                    HAL_UART_Receive_IT(&huart5, (unsigned char*)rx_data, 25);
+                    UARTState = URecieve;
+                }
+                else
+                {
+                    UARTState = UWait;
+                }
+        	}
+        	else if (pzem == 3)
+        	{
+                if (HAL_UART_Transmit(&huart3, request, 8, 50) == HAL_OK)
+                {
+                	pzem_active = 3;
+                    HAL_UART_Receive_IT(&huart3, (unsigned char*)rx_data, 25);
+                    UARTState = URecieve;
+                }
+                else
+                {
+                    UARTState = UWait;
+                }
+        	}
         break;
         case URecieve:
             if (rx_done)
             {
             	rx_done = 0;
+            	pzem = pzem + 1;
                 UARTState = UWait;
             }
+            else
+            {
+            	timeout = timeout + 1;
+            	if (timeout >= 2)
+            	{
+            		timeout = 0;
+            		UARTState = UWait;
+            		pzem = pzem + 1;
+            	}
+            }
+        	if (pzem > 3)
+        	{
+        		pzem = 1;
+        	}
         break;
     }
 }
 
 void CState_Machine(void)
 {
+    unsigned long raw_voltage;
+    unsigned long raw_current;
+    unsigned long raw_watts;
+    unsigned long raw_watthours;
     switch (CalcState){
         case CInit:
 
         CalcState = CWait;
         break;
         case CWait:
-            if (rx_done) {
+            if (rx_done)
+            {
                 CalcState = CCompute;
             }
         break;
         case CCompute:
         	if (rx_data[0] == 0x01 && rx_data[1] == 0x04 && rx_data[2] == 0x14)
         	{
-        		voltage = ((rx_data[3] << 8) | rx_data[4])/10;
-        		current = ((rx_data[7] << 8 | rx_data[8]) << 16) | ((rx_data[5] << 8) | rx_data[6]);
-        		watts = ((rx_data[11] << 8 | rx_data[12]) << 16) | ((rx_data[9] << 8) | rx_data[10])/10;
-        		watthours = ((rx_data[15] << 8 | rx_data[16]) << 16) | ((rx_data[13] << 8) | rx_data[14]);
+        		raw_voltage = ((rx_data[3] << 8) | rx_data[4]);
+        		raw_current = ((rx_data[7] << 8 | rx_data[8]) << 16) | ((rx_data[5] << 8) | rx_data[6]);
+        		raw_watts = ((rx_data[11] << 8 | rx_data[12]) << 16) | ((rx_data[9] << 8) | rx_data[10]);
+        		raw_watthours = ((rx_data[15] << 8 | rx_data[16]) << 16) | ((rx_data[13] << 8) | rx_data[14]);
+
+        		if (pzem_active == 1)
+        		{
+        			voltage = raw_voltage | 0x00000000;
+        			current = raw_current | 0x00000000;
+        			watts = raw_watts | 0x00000000;
+        			watthours = raw_watthours | 0x00000000;
+        		}
+        		else if (pzem_active == 2)
+        		{
+        			voltage = raw_voltage | 0x40000000;
+        			current = raw_current | 0x40000000;
+        			watts = raw_watts | 0x40000000;
+        			watthours = raw_watthours | 0x40000000;
+        		}
+        		else if (pzem_active == 3)
+        		{
+        			voltage = raw_voltage | 0x80000000;
+        			current = raw_current | 0x80000000;
+        			watts = raw_watts | 0x80000000;
+        			watthours = raw_watthours | 0x80000000;
+        		}
         	}
         	CalcState = CWait;
         break;
@@ -161,6 +240,23 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_I2C1_Init(void)
+{
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
   {
     Error_Handler();
   }
@@ -215,26 +311,25 @@ static void MX_USART1_UART_Init(void)
 
 }
 
-static void MX_USART2_UART_Init(void)
+static void MX_UART5_Init(void)
 {
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
+  huart5.Instance = UART5;
+  huart5.Init.BaudRate = 9600;
+  huart5.Init.WordLength = UART_WORDLENGTH_8B;
+  huart5.Init.StopBits = UART_STOPBITS_1;
+  huart5.Init.Parity = UART_PARITY_NONE;
+  huart5.Init.Mode = UART_MODE_TX_RX;
+  huart5.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart5.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart5) != HAL_OK)
   {
     Error_Handler();
   }
-
 }
 static void MX_USART3_UART_Init(void)
 {
   huart3.Instance = USART3;
-  huart3.Init.BaudRate = 115200;
+  huart3.Init.BaudRate = 9600;
   huart3.Init.WordLength = UART_WORDLENGTH_8B;
   huart3.Init.StopBits = UART_STOPBITS_1;
   huart3.Init.Parity = UART_PARITY_NONE;
@@ -259,16 +354,31 @@ static void MX_GPIO_Init(void)
 
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_10|GPIO_PIN_11, GPIO_PIN_SET);
+
   GPIO_InitStruct.Pin = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
 }
 void Error_Handler(void)
